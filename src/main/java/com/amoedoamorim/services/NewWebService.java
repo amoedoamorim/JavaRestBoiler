f70aafd3a5901b3c34f0fd8b924a7f1a30c6c73c
Alexandre Amoedo Amorim <amoedoamorim@gmail.com>
2016-11-23 01:23:30 -0300

    Added Jackson Json provider

--- src/main/java/com/amoedoamorim/services/NewWebService.java
+++ src/main/java/com/amoedoamorim/services/NewWebService.java
@@ -1,17 +1,19 @@
 package com.amoedoamorim.services;
 
+import com.amoedoamorim.javarestboiler.model.*;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
 import org.springframework.stereotype.Service;
 
 @Service
 @Path("/newservice")
 public class NewWebService {
     @GET
-    @Produces("text/plain")
-    public String hello(@QueryParam("budget") final Long idBudget) {
-        return "Hello World";    
+    @Produces("application/json;charset=UTF-8")
+    public Response hello(@QueryParam("budget") final Long idBudget) {
+        return Response.ok(new Student()).build();
     }
 }
